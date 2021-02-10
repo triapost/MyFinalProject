4f0b9c23fc3ff41a77a0c356e0198b008d8cde9f
Korkut Sarıçayır <61970387+triapost@users.noreply.github.com>
2021-02-10 13:07:50 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            return _productDal.GetAll();
+
+        }
+    }
+}
